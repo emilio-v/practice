09ad59d4544531bf7fa76954e55fdd7e8c81ce60
emilio-v <emilio_785@hotmail.com>
2021-07-25 00:59:49 -0500

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import BasicTypes from './typescript/BasicTypes';
 
 const App = () => {
   return (
     <div className="mt-2">
-      <h1>Introduce to TS-React</h1>
+      <h1>Introduction to TS-React</h1>
+      <BasicTypes />
     </div>
   );
 };
